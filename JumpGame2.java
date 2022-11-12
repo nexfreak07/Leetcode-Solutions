1b59ceedd07b3223b7c2927e6623371620025323
Akash Dasgupta <akashakxy@gmail.com>
2022-11-13 03:57:46 +0530

    Update JumpGame2.java

--- JumpGame2.java
+++ JumpGame2.java
@@ -1,17 +1,22 @@
 public class JumpGame2 {
+   /* Either you run a loop till N-1, because when it comes to end of the array
+the i is equal to currentReach, and hence whatever the maxReach is found fom last index
+will be updated and jump++ will be done which is not really required  */
     public int jump(int[] nums) {
+        int currentReach = 0; // Variable to store reach after a jump
+        int maxReach = 0; // Variable Stores maximum reach if any, towards left of currentReach
         int jump = 0;
-        int maxReach = 0;
-        int pos = 0;
-        for(int i=0; i<nums.length-1;i++)
+
+        for(int i=0; i<nums.length-1; i++) //Loop To Explore Maximum Reach till Curret Reach
         {
-            maxReach = Math.max(maxReach, i+nums[i]);
-            if(pos==i)
+            maxReach = Math.max(maxReach, i+nums[i]); //Finding Maximum Reaching Element till current Reach
+            if(currentReach==i) //Loop Reached till currentReach
             {
-                pos = maxReach;
-                jump++;
+                currentReach = maxReach; // Taking Maximum Jump, which is found till currentReach
+                jump++; //Counting Jump
             }
         }
         return jump;
+        
     }
 }
